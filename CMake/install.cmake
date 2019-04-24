edda0e11d2fd66e287295d22fffdba6ec3cb38cd
Ryan Pavlik <rpavlik@gmail.com>
2019-04-24 12:35:07 -0500

    fix installation

--- CMake/install.cmake
+++ CMake/install.cmake
@@ -1,28 +1,41 @@
 include(CMakePackageConfigHelpers)
-configure_package_config_file(pipedreamConfig.cmake.in
-    ${CMAKE_CURRENT_BINARY_DIR}/pipedreamConfig.cmake
-    INSTALL_DESTINATION ${INSTALL_LIB_DIR}/pipedream/cmake
-    PATH_VARS INSTALL_INCLUDE_DIR
-    )
+
+configure_file(pipedreamConfig.cmake.in
+  ${CMAKE_CURRENT_BINARY_DIR}/pipedreamConfig.cmake
+  @ONLY
+  )
+
+message(STATUS "Install Prefix: ${CMAKE_INSTALL_PREFIX}")
 
 write_basic_package_version_file(
-    ${CMAKE_CURRENT_BINARY_DIR}/pipedreamConfigVersion.cmake
-    VERSION 0.1
-    COMPATIBILITY SameMajorVersion
-    )
+  ${CMAKE_CURRENT_BINARY_DIR}/pipedreamConfigVersion.cmake
+  VERSION 0.1
+  COMPATIBILITY SameMajorVersion
+  )
+
+install(TARGETS pipedream EXPORT pipedreamTargets
+  INCLUDES DESTINATION include
+  )
+
+export(EXPORT pipedreamTargets
+  FILE "${CMAKE_CURRENT_BINARY_DIR}/pipedreamTargets.cmake"
+  )
+
+install(EXPORT pipedreamTargets
+  FILE pipedreamTargets.cmake
+  DESTINATION lib/cmake/pipedream
+  )
 
 file(GLOB PIPEDREAM_FILES
-    ${CMAKE_CURRENT_SOURCE_DIR}/include/*.hpp
-    )
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/*.hpp
+  )
 file(GLOB PIPEDREAM_DIRS
-    ${CMAKE_CURRENT_SOURCE_DIR}/include/piped
-    )
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/piped
+  )
 
 install(FILES ${PIPEDREAM_FILES} DESTINATION ${INSTALL_INCLUDE_DIR})
 install(DIRECTORY ${PIPEDREAM_DIRS} DESTINATION ${INSTALL_INCLUDE_DIR})
 install(FILES
-    ${CMAKE_CURRENT_BINARY_DIR}/pipedreamConfig.cmake
-    ${CMAKE_CURRENT_BINARY_DIR}/pipedreamConfigVersion.cmake
-    DESTINATION
-    ${INSTALL_LIB_DIR}/pipedream/cmake)
-
+  "${CMAKE_CURRENT_BINARY_DIR}/pipedreamConfig.cmake"
+  "${CMAKE_CURRENT_BINARY_DIR}/pipedreamConfigVersion.cmake"
+  DESTINATION "lib/cmake/pipedream")
