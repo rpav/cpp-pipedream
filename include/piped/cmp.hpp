91d95c48c0a46c1b5ff7287ad994a44d6b01a0da
Ryan Pavlik <rpavlik@gmail.com>
2019-08-23 16:39:41 -0500

    not_in and map_value

--- include/piped/cmp.hpp
+++ include/piped/cmp.hpp
@@ -11,7 +11,7 @@ namespace piped {
 
 namespace detail {
 
-template<typename... Ts>
+template<bool NOT, typename... Ts>
 struct in_ {
     std::tuple<Ts...> vs;
 
@@ -20,7 +20,7 @@ struct in_ {
     template<typename T>
     inline bool process(const T& v)
     {
-        return std::apply([&](auto&&... vs) -> bool { return (... || (v == vs)); }, vs);
+        return std::apply([&](auto&&... vs) -> bool { return (... || (v == vs)) != NOT; }, vs);
     }
 };
 
@@ -43,7 +43,69 @@ if(x | in(1, 2, 3)) {
 template<typename... Ts>
 auto in(Ts&&... vs)
 {
-    return detail::in_<Ts...>{std::forward<Ts>(vs)...};
+    return detail::in_<false, Ts...>{std::forward<Ts>(vs)...};
 }
 
+/**
+   @brief Test whether the input is in *not* in the specified list of values.
+   @param vs A list of values which can compare by == to the input.
+   @ingroup comparison
+
+   @details This will statically expand to a comparison of `... || v == vs`,
+   and negate the result.  This is for convenience and deemed less ugly than `if(!(x | in(...)))`.
+
+   Example:
+@code{.cpp}
+if(x | not_in(1, 2, 3)) {
+  // ...
+}
+@endcode
+ */
+template<typename... Ts>
+auto not_in(Ts&&... vs)
+{
+    return detail::in_<true, Ts...>{std::forward<Ts>(vs)...};
+}
+
+namespace detail {
+template<typename K, typename V>
+auto map_value_test(const K& test, const std::pair<K, V>& p, const V& def)
+{
+    if(test == p.first) return p.second;
+
+    return def;
+}
+
+template<typename K, typename V, typename... Ts>
+auto map_value_test(const K& test, const std::pair<K, V>& p, const Ts&... vs)
+{
+    static_assert(sizeof...(Ts) != 0 && "Default value required for map_value!");
+    if(test == p.first) return p.second;
+    return map_value_test(test, vs...);
+}
+} // namespace detail
+
+/**
+   @brief Map the input value to another value, options specified as pairs, with a default at the end
+   @param vs A list of one or more `pair<K,V>` ending with a `V default_value`.
+   @ingroup comparison
+
+   @details Example:
+@code{.cpp}
+int foo(int x) {
+   using p = std::pair<int,int>;
+
+   return x | map_value<int,int>(
+                p(0,1),
+                p(1,2),
+                p(2,3),
+                4);              // Default value if `x` does not match
+}
+@endcode
+   */
+template<typename K, typename V, typename... Ts>
+auto map_value(const Ts&... vs)
+{
+    return simple_operation([=](const K& test) { return detail::map_value_test<K, V>(test, vs...); });
+}
 } // namespace piped
